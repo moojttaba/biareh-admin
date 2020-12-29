fe6c19eb6577ef8f96192cf77ead0b86663d70f4
test <test@gmail.com>
2020-12-29 12:23:52 +0330

    e

--- src/redux/API/API.selectors.js
+++ src/redux/API/API.selectors.js
@@ -1,33 +1,9 @@
 import { createSelector } from "reselect";
 
-const selectUi = (state) => state.ui;
+const selectAPI = (state) => state.API;
 
 export const selectDrawerHidden = createSelector(
-  [selectUi],
-  (state) => state.hidden
+  [selectAPI],
+  (state) => state.request
 );
 
-export const selectDrawerOrders = createSelector(
-  [selectUi],
-  (state) => state.orders
-);
-
-export const selectDrawerProducts = createSelector(
-  [selectUi],
-  (state) => state.products
-);
-
-export const selectDrawerAnalysis = createSelector(
-  [selectUi],
-  (state) => state.analysis
-);
-
-export const selectDrawerMarketing = createSelector(
-  [selectUi],
-  (state) => state.marketing
-);
-
-export const selectDrawerStore = createSelector(
-  [selectUi],
-  (state) => state.store
-);
