115c5d6ff05b92a11aebaca88f552bce09bd155f
test <test@gmail.com>
2020-12-29 12:07:38 +0330

    e

--- src/redux/API/API.actions.js
+++ src/redux/API/API.actions.js
@@ -1,11 +1,12 @@
 import APIActionTypes from "./API.types";
 import axios from "axios";
 
-const ROOT_URL = "http://reduxblog.herokuapp.com/api";
-const API_KEY = "?key=PAPERCLIP1234";
+const ROOT_URL = "/api/v1/products";
+//const API_KEY = "?key=PAPERCLIP1234";
 
 export const fetchPosts = () => {
-  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+  // const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+  const request = axios.get(`${ROOT_URL}`);
 
   return {
     type: APIActionTypes.FETCH_POSTS,
